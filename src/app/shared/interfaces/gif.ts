acfba49ac496509a86f704297810917411cc43dc
mitrasingh <mitra.coder@gmail.com>
2025-01-22 08:17:47 -0500

    feat: interface for Gif data

---
+++ src/app/shared/interfaces/gif.ts
@@ -0,0 +1,11 @@
+// GIF data type receied from Reddit API
+
+export interface Gif {
+  src: string;
+  author: string;
+  name: string;
+  permalink: string;
+  title: string;
+  thumbnail: string;
+  comments: number;
+}
