4307d251e42628d1093173c7b34a5a40a192cfe4
mitrasingh <mitra.coder@gmail.com>
2025-01-23 19:32:55 -0500

    feat: import GiftListComponent, add to template and bind input property with computed property from RedditService component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { RedditService } from '../shared/data/reddit.service';
+import { GiftListComponent } from './ui/gif-list.component';
 @Component({
   selector: 'app-home',
-  template: `<h1>Home</h1>`,
+  template: `
+    <h1>Home</h1>
+    <app-gif-list [gifs]="this.redditService.gifs()"></app-gif-list>
+  `,
+  imports: [GiftListComponent],
 })
 export default class HomeComponent {
   redditService = inject(RedditService);
