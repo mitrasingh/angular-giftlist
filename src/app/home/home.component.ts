27af21977be78574b9faaf044105d8784031a064
mitrasingh <mitra.coder@gmail.com>
2025-01-23 19:25:41 -0500

    feat: inject RedditService into component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { RedditService } from '../shared/data/reddit.service';
 @Component({
   selector: 'app-home',
   template: `<h1>Home</h1>`,
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  redditService = inject(RedditService);
+}
