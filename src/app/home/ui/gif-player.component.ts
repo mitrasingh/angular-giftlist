8db1931fb4b02101cc438912ce8cead4cbee2960
mitrasingh <mitra.coder@gmail.com>
2025-01-25 08:51:26 -0500

    feat: create interface and setup state of type GifPlayerState

--- src/app/home/ui/gif-player.component.ts
+++ src/app/home/ui/gif-player.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface GifPlayerState {
+  playing: boolean;
+  status: 'initial' | 'loading' | 'loaded';
+}
 
 @Component({
   selector: 'app-gif-player',
   template: `<p>Gif Player</p>`,
 })
-export class GifPlayerComponent {}
+export class GifPlayerComponent {
+  state = signal<GifPlayerState>({
+    playing: false,
+    status: 'initial',
+  });
+}
