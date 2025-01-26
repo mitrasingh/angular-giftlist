825f01197e37db06a5da6a2c5ff5a4e009fb5c85
mitrasingh <mitra.coder@gmail.com>
2025-01-26 11:57:59 -0500

    feat: load spinner via angular material, include conditional statement in template whether status is loading to display load spinner

--- src/app/home/ui/gif-player.component.ts
+++ src/app/home/ui/gif-player.component.ts
@@ -1,17 +1,26 @@
-import { Component, signal } from '@angular/core';
-
+import { Component, computed, signal } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 interface GifPlayerState {
   playing: boolean;
   status: 'initial' | 'loading' | 'loaded';
 }
-
 @Component({
   selector: 'app-gif-player',
-  template: `<p>Gif Player</p>`,
+  template: `
+    @if (status() === 'loading') {
+    <mat-progress-spinner />
+    }
+  `,
+  imports: [MatProgressSpinnerModule],
 })
 export class GifPlayerComponent {
+  // state
   state = signal<GifPlayerState>({
     playing: false,
     status: 'initial',
   });
+
+  // selectors
+  playing = computed(() => this.state().playing);
+  status = computed(() => this.state().status);
 }
