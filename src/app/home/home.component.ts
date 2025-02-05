b304953fd8474705cf57f9c64c62f5771de87401
mitrasingh <mitra.coder@gmail.com>
2025-02-04 22:48:32 -0500

    feat: import infinite scroll, add infinite scroll attribute and event to template which emits our next pagination$

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { RedditService } from '../shared/data/reddit.service';
 import { GifListComponent } from './ui/gif-list.component';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 @Component({
   selector: 'app-home',
   template: `
     <h1>Home</h1>
     <app-gif-list
       [gifs]="redditService.gifs()"
+      infiniteScroll
+      (scrolled)="redditService.pagination$.next(redditService.lastKnownGif())"
       class="grid-container"
     ></app-gif-list>
   `,
-  imports: [GifListComponent],
+  imports: [GifListComponent, InfiniteScrollDirective],
 })
 export default class HomeComponent {
   redditService = inject(RedditService);
