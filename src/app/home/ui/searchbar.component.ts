9dbff2741279ff09d6f5ffebded561146dfa81a7
mitrasingh <mitra.coder@gmail.com>
2025-02-09 13:34:37 -0500

    feat: add material ui components for styling

--- src/app/home/ui/searchbar.component.ts
+++ src/app/home/ui/searchbar.component.ts
@@ -1,13 +1,32 @@
 import { Component, input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-search-bar',
   template: `
-    Search Sub Reddit Gif:
-    <input type="text" [formControl]="subredditFormControl()" />
+    <mat-toolbar>
+      <mat-form-field appearance="outline">
+        <input
+          matInput
+          placeholder="subreddit..."
+          type="text"
+          [formControl]="subredditFormControl()"
+        />
+        <mat-icon matSuffix>search</mat-icon>
+      </mat-form-field>
+    </mat-toolbar>
   `,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
 export class SearchBarComponent {
   subredditFormControl = input.required<FormControl>();
