b54c794afc71ba4daf4aacae9be4b34761d7797f
mitrasingh <mitra.coder@gmail.com>
2025-02-10 22:13:39 -0500

    feat: import search bar and bind input property to reddit service property

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,17 +2,32 @@ import { Component, inject } from '@angular/core';
 import { RedditService } from '../shared/data/reddit.service';
 import { GifListComponent } from './ui/gif-list.component';
 import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
+import { SearchBarComponent } from './ui/searchbar.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 @Component({
   selector: 'app-home',
   template: `
+    <app-search-bar [subredditFormControl]="redditService.subredditFormControl">
+    </app-search-bar>
+
+    @if (redditService.loading()) {
+    <mat-progress-spinner mode="indeterminate" diameter="50" />
+    } @else {
     <app-gif-list
       [gifs]="redditService.gifs()"
-      infiniteScroll
+      infinite-scroll
       (scrolled)="redditService.pagination$.next(redditService.lastKnownGif())"
       class="grid-container"
-    ></app-gif-list>
+    />
+    }
   `,
-  imports: [GifListComponent, InfiniteScrollDirective],
+  imports: [
+    GifListComponent,
+    InfiniteScrollDirective,
+    SearchBarComponent,
+    MatProgressSpinnerModule,
+  ],
 })
 export default class HomeComponent {
   redditService = inject(RedditService);
