d15f354322f3077b981cd5a48216ef922178a26f
mitrasingh <mitra.coder@gmail.com>
2025-02-11 22:40:03 -0500

    refactor: import / rename window injection

--- src/app/home/ui/gif-list.component.ts
+++ src/app/home/ui/gif-list.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, input } from '@angular/core';
 import { Gif } from '../../shared/interfaces';
 import { GifPlayerComponent } from './gif-player.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { WINDOW } from '../../shared/utils/injection-tokens';
 
 @Component({
   selector: 'app-gif-list',
@@ -9,7 +10,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     @for (gif of gifs(); track gif.permalink){
     <div>
       <mat-toolbar>
-        <span>{{ gif.name }}</span>
+        <span>{{ gif.title }}</span>
       </mat-toolbar>
       <app-gif-player
         [src]="gif.src"
@@ -21,6 +22,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   imports: [GifPlayerComponent, MatToolbarModule],
 })
 export class GifListComponent {
-  windowToken = inject(Window);
+  window = inject(WINDOW);
   gifs = input.required<Gif[]>();
 }
