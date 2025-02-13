e4da8736d0bf7c4407f5a486a5e4a11c98470aae
mitrasingh <mitra.coder@gmail.com>
2025-02-13 18:55:14 -0500

    feat: add styling to component

--- src/app/home/ui/gif-list.component.ts
+++ src/app/home/ui/gif-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject, input } from '@angular/core';
 import { Gif } from '../../shared/interfaces';
 import { GifPlayerComponent } from './gif-player.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { WINDOW } from '../../shared/utils/injection-tokens';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-gif-list',
@@ -29,7 +30,31 @@ import { MatIconModule } from '@angular/material/icon';
     <p>No gifs found</p>
     }
   `,
-  imports: [GifPlayerComponent, MatToolbarModule, MatIconModule],
+  imports: [
+    GifPlayerComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  styles: [
+    `
+      div {
+        margin: 1rem;
+        filter: drop-shadow(0px 0px 6px #0e0c1ba8);
+      }
+
+      mat-toolbar {
+        white-space: break-spaces;
+      }
+
+      p {
+        font-size: 2em;
+        width: 100%;
+        text-align: center;
+        margin-top: 4rem;
+      }
+    `,
+  ],
 })
 export class GifListComponent {
   window = inject(WINDOW);
