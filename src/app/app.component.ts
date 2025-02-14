2b5eb39906829b704ddc16006b4c0a6050ccd71f
mitrasingh <mitra.coder@gmail.com>
2025-02-14 09:11:52 -0500

    feat: inject RedditService into component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RedditService } from './shared/data/reddit.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { RouterOutlet } from '@angular/router';
   template: `<router-outlet />`,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent {
+  redditService = inject(RedditService);
+}
